ffcc3514e53f0af4e6b7df8c731e5df6148a0a94
Phil Hagelberg <phil@hagelb.org>
2008-03-21 16:00:43 -0700

    added a post

--- serve.rb
+++ serve.rb
@@ -4,12 +4,12 @@ require 'rubygems'
 require 'mongrel'
 
 config = Mongrel::Configurator.new :host => "127.0.0.1" do
-  listener :port => 3000 do
+  listener :port => 3001 do
     uri "/", :handler => Mongrel::DirHandler.new("public")
     # TODO: content-negotiate to transform "contact" into "contact.html"
   end
   run
 end
 
-puts "Serving on port 3000..."
+puts "Serving on port 3001..."
 config.join
